5a9989e4566fa75295c28ac67abc88820fcb97b6
Anwesh43 <anweshthecool0@gmail.com>
2020-12-15 22:36:54 +0530

    adding VDLJNode class

--- index.ts
+++ index.ts
@@ -148,3 +148,45 @@ class Animator {
         }
     }
 }
+
+class VDLJNode {
+
+    next : VDLJNode 
+    prev : VDLJNode 
+    state : State = new State()
+
+    constructor(private i : number) {
+        this.addNeighbor()
+    }
+
+    addNeighbor() {
+        if (this.i < colors.length - 1) {
+            this.next = new VDLJNode(this.i + 1)
+            this.next.prev = this 
+        }
+    }
+
+    draw(context : CanvasRenderingContext2D) {
+        DrawingUtil.drawDLJNode(context, this.i, this.state.scale)
+    }
+
+    update(cb  : Function) {
+        this.state.update(cb)
+    }
+
+    startUpdating(cb : Function) {
+        this.state.startUpdating(cb)
+    }
+
+    getNext(dir : number, cb : Function) : VDLJNode {
+        var curr : VDLJNode = this.prev 
+        if (dir == 1) {
+            curr = this.next 
+        }
+        if (curr) {
+            return curr 
+        }
+        cb()
+        return this 
+    }
+}
