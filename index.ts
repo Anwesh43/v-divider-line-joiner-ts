7684aa95c4631b17550f00f97f37ac3dd3ed9f0d
Anwesh43 <anweshthecool0@gmail.com>
2020-12-15 22:48:51 +0530

    adding VDividerLineJoiner class

--- index.ts
+++ index.ts
@@ -190,3 +190,26 @@ class VDLJNode {
         return this 
     }
 }
+
+class VDividerLineJoiner {
+
+    curr : VDLJNode = new VDLJNode(0)
+    dir : number = 1 
+
+    draw(context : CanvasRenderingContext2D) {
+        this.curr.draw(context)
+    }
+
+    update(cb : Function) {
+        this.curr.update(() => () => {
+            this.curr = this.curr.getNext(this.dir, () => {
+                this.dir *= -1
+            })
+            cb()
+        })
+    }
+
+    startUpdating(cb : Function) {
+        this.curr.startUpdating(cb)
+    }
+}
