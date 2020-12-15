e29ab54d291a7184ebeccfd57e2dcf7725dae7f7
Anwesh43 <anweshthecool0@gmail.com>
2020-12-15 20:56:10 +0530

    adding DrawingUtil class

--- index.ts
+++ index.ts
@@ -6,7 +6,7 @@ const strokeFactor : number = 90
 const sizeFactor : number = 4.8  
 const delay : number = 20 
 const backColor : string = "#BDBDBD"
-const color : Array<string> = [
+const colors : Array<string> = [
     "#F44336",
     "#3F51B5",
     "#4CAF50",
@@ -26,5 +26,50 @@ class ScaleUtil {
 
     static sinify(scale : number)  : number {
         return Math.sin(scale * Math.PI)
+    }
+}
+
+class DrawingUtil {
+
+    static drawLine(context : CanvasRenderingContext2D, x1 : number, y1 : number, x2 : number, y2 : number) {
+        context.beginPath()
+        context.moveTo(x1, y1)
+        context.lineTo(x2, y2)
+        context.stroke()
+    }
+    
+    static drawEndingLine(context : CanvasRenderingContext2D, xSize : number, ySize : number, sc1 : number, sc2 : number) {
+        DrawingUtil.drawLine(context, xSize * sc2, ySize * sc2, xSize * sc1, ySize * sc1)
+    }
+
+    static drawDividerLineJoiner(context : CanvasRenderingContext2D, scale : number) {
+        const size : number = Math.min(w, h) / sizeFactor 
+        const sc1 : number = ScaleUtil.divideScale(scale, 0, 2)
+        const sc2 : number = ScaleUtil.divideScale(scale, 1, 2)
+        const xGap : number = size / parts 
+        context.save()
+        context.translate(w / 2, h / 2)
+        for (var j = 0; j < 2; j++) {
+            context.save()
+            context.scale(1 - 2 * j, 1)
+            DrawingUtil.drawEndingLine(context, -size, -size, sc1, sc2)
+            for (var k = 0; k < parts; k++) {
+                const sc1k : number = ScaleUtil.divideScale(sc1, k, parts)
+                const sc2k : number = ScaleUtil.divideScale(sc2, k, parts)
+                context.save()
+                context.translate(xGap * k, 0)
+                DrawingUtil.drawEndingLine(context, 0, -xGap * k, sc1k, sc2k)
+                context.restore()
+            }
+            context.restore()
+        }
+        context.restore()
+    }
+
+    static drawDLJNode(context : CanvasRenderingContext2D, i : number, scale : number) {
+        context.lineCap = 'round'
+        context.lineWidth = Math.min(w, h) / strokeFactor 
+        context.strokeStyle = colors[i]
+        DrawingUtil.drawDividerLineJoiner(context, scale)
     }
 }
